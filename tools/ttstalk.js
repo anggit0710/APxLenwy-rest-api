ba0a5837fba624d8023fe1801b1139281b10f960
AP <59376654+anggit0710@users.noreply.github.com>
2024-10-23 00:27:01 +0700

    Add files via upload

---
+++ tools/ttstalk.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+
+async function tiktokStalk(username) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let res = await fetch(
+        "https://tools.revesery.com/stalktk/revesery.php?username=" +
+          encodeURI(username.replace(/[^\w\d]/gi, ""))
+      );
+      if (!res.ok) return reject("User Not Found");
+      res = await res.json();
+      delete res.base64;
+      console.log(res);
+      resolve(res);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+// Endpoint tiktokstalk
+  app.get('/tools/ttstalk', async (req, res) => {
+    try {
+      const { text } = req.query;
+      if (!text) {
+        return res.status(400).json({ error: 'Parameter "text" Tidak Ditemukan, Tolong Masukkan Perintah' });
+      }
+      const response = await tiktokStalk(text);
+      res.status(200).json({
+        status: 200,
+        creator: "AP",
+        data: response
+      });
+    } catch (error) {
+      res.status(500).json({ error: `Internal server error: ${error.message}` });
+    }
+  });
+};
